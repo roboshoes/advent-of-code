9b17e67572d9a4fc5e2e6099e4c7f4c2b46175e6
Mathias Paumgarten <mail@mathias-paumgarten.com>
2015-12-10 08:48:57 -0800

    adds day 10

---
+++ day-10/part-2.js
@@ -0,0 +1,34 @@
+var input = "1113122113";
+
+
+function lookAndSay( code ) {
+
+    var accumilator = "";
+    var buffer = "";
+
+    function flush() {
+        if ( buffer.length > 0 ) {
+            accumilator += buffer.length.toString() + buffer[ 0 ];
+        }
+
+        buffer = "";
+    }
+
+    for ( var j = 0; j < code.length; j++ ) {
+        var letter = code[ j ];
+
+        if ( buffer[ 0 ] !== letter ) flush();
+
+        buffer += letter;
+    }
+
+    flush();
+
+    return accumilator;
+}
+
+for ( var i = 0; i < 50; i++ ) {
+    input = lookAndSay( input );
+}
+
+console.log( input.length );
