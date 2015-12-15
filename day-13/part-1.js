f4802e0be124f8744f46f84618209dd2d3a375e1
Mathias Paumgarten <mail@mathias-paumgarten.com>
2015-12-14 17:16:52 -0800

    adds day 13

---
+++ day-13/part-1.js
@@ -0,0 +1,67 @@
+var input = require( "fs" )
+    .readFileSync( __dirname + "/input.txt", "utf-8" )
+    .split( "\n" );
+
+var characters = {};
+var highest = - Infinity;
+
+function addCharacter( active, value, passive ) {
+    if ( ! characters[ active ] ) {
+        characters[ active ] = {};
+    }
+
+    characters[ active ][ passive ] = value;
+}
+
+input.forEach( function( line ) {
+
+    var parts = line.split( " " );
+
+    var active = parts[ 0 ];
+    var factor = parts[ 2 ] === "gain" ? 1 : -1;
+    var value = parseInt( parts[ 3 ], 10 ) * factor;
+    var passive = parts.pop();
+
+    passive = passive.substr( 0, passive.length - 1 );
+
+    addCharacter( active, value, passive );
+
+} );
+
+function sum( order ) {
+    return order.reduce( function( previous, current, index ) {
+        var right = order[ ( index + 1 ) % order.length ];
+        var left = order[ index === 0 ? order.length - 1 : index - 1 ];
+
+        return previous + characters[ current ][ left ] + characters[ current ][ right ];
+    }, 0 );
+}
+
+function withoutIndex( array, index ) {
+    var copy = [];
+
+    for ( var i = 0; i < array.length; i++ ) {
+        if ( i !== index ) copy.push( array[ i ] );
+    }
+
+    return copy;
+}
+
+function permutation( defined, variable ) {
+
+    if ( variable.length === 0 ) {
+
+        highest = Math.max( sum( defined ), highest );
+
+    } else {
+
+        for ( var i = 0; i < variable.length; i++ ) {
+            permutation( defined.concat( variable[ i ] ), withoutIndex( variable, i ) );
+        }
+
+    }
+}
+
+permutation( [], Object.keys( characters ) );
+
+console.log( highest );
