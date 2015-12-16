8e84dd4ae3f1bafbc5aaab04da7d2be6a42a1be6
Mathias Paumgarten <mail@mathias-paumgarten.com>
2015-12-16 08:46:26 -0800

    adds day 16

---
+++ day-16/part-1.js
@@ -0,0 +1,31 @@
+var fs = require( "fs" );
+
+var input = fs.readFileSync( __dirname + "/input.txt", "utf-8" ).split( "\n" );
+var mfcsam = fs.readFileSync( __dirname + "/mfcsam.txt", "utf-8" ).split( "\n" );
+
+var clues = {};
+
+mfcsam.forEach( function( line ) {
+    var parts = line.split( ":" );
+
+    clues[ parts[ 0 ] ] = parseInt( parts[ 1 ].trim(), 10 );
+} );
+
+input.forEach( function( line ) {
+    var name = line.substr( 0, line.indexOf( ":" ) );
+    var number = name.split( " " ).pop();
+    var facts = line.substr( name.length + 2 ).split( "," );
+
+    var match = facts.reduce( function( previous, fact ) {
+
+        var parts = fact.trim().split( ":" );
+
+        var key = parts[ 0 ];
+        var value = parseInt( parts[ 1 ].trim(), 0 );
+
+        return previous && ( clues[ key ] === value );
+
+    }, true );
+
+    if ( match ) console.log( number );
+} );
