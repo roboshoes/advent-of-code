ae7595658a0f4dbf3e2f8369097639a52bea09b2
Mathias Paumgarten <mail@mathias-paumgarten.com>
2015-12-21 22:17:59 -0800

    adds day 20

--- day-20/part-2.js
+++ day-20/part-2.js
@@ -8,8 +8,8 @@ for ( var i = 1; i <= elves; i++ ) {
     if ( ! houses[ i ] ) houses[ i ] = 0;
 
     for ( var j = 1; j <= i; j++ ) {
-        if ( i % j === 0 ) {
-            houses[ i ] += j * 10;
+        if ( i % j === 0 && i / j <= 50 ) {
+            houses[ i ] += j * 11;
         }
 
         if ( houses[ i ] >= input ) {
